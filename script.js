359782b1973ac59d222eeae10c18d6e0f02db36a
Theo Gkanoudis <tgk@abakion.com>
2025-06-17 00:16:29 +0200

    finished calendar functionality

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 import { getCookie, setCookie } from "./utils.js";
 
-const weekBeginsOnMonday = true; // Set to false if week starts on Sunday
+const weekBeginsOnMonday = true;
 
 $(function () {
    initView();
@@ -18,45 +18,104 @@ function initView() {
 }
 
 function initCalendar() {
-   const date = new Date();
-   const month = date.getMonth();
-   const year = date.getFullYear();
-
-   const monthName = date.toLocaleString("default", { month: "long" });
-   $(currentMonth).text(monthName).removeClass("loading");
-
-   let firstDay = new Date(year, month, 1).getDay();
-   const daysInMonth = new Date(year, month, 0).getDate();
-
-   if (weekBeginsOnMonday) {
-      firstDay = (firstDay + 6) % 7; // Adjust for Monday start
-   }
-   //get previous month
-   const prevMonth = new Date(year, month - 1, 0);
-   const prevDaysInMonth = prevMonth.getDate();
-
-   const day = $('<div class="day">');
-   let daySlots = 42; // 6 rows of 7 days
-   if (firstDay + daysInMonth < 36) {
-      daySlots = 35; // 5 rows of 7 days
-   }
-   for (let i = 0; i < daySlots; i++) {
-      const currentDay = day.clone();
-      let dayNumber = i - firstDay + 1; // Adjust for first day offset
-      if (i < firstDay) {
-         dayNumber = prevDaysInMonth - (firstDay - i) + 1; // Previous month days
-         currentDay.addClass("prev");
-      } else if (i >= firstDay + daysInMonth) {
-         dayNumber = dayNumber - daysInMonth; // Next month days
-         currentDay.addClass("next");
-      }
-      currentDay.text(dayNumber);
-      $(calendar).append(currentDay);
-   }
+   setMonth();
 }
 
 function initListeners() {
    $(timeframeSelector).find("button").on("click", setTimeframe);
+   $(calendar).on("click", changeDay);
+   $(prevMonth).on("click", () =>
+      setMonth(parseInt($(calendar).attr("distance")) - 1)
+   );
+   $(nextMonth).on("click", () =>
+      setMonth(parseInt($(calendar).attr("distance")) + 1)
+   );
+}
+
+function setMonth(distance = 0) {
+   const date = new Date();
+   const month = date.getMonth();
+   const year = date.getFullYear();
+
+   //get the selected month and set name
+   const selectedMonth = new Date(year, month + distance, 1);
+   const monthName = selectedMonth.toLocaleString("default", { month: "long" });
+   const yearNo = selectedMonth.getFullYear();
+   const monthNo = selectedMonth.getMonth();
+   const dayNo = date.getDate();
+   $(currentMonth).text(monthName).removeClass("loading");
+   $(currentYear).text(yearNo).removeClass("loading");
+
+   //set the calendar content
+   $(calendar).empty();
+
+   let firstDay = selectedMonth.getDay();
+   if (weekBeginsOnMonday) {
+      firstDay = (firstDay + 6) % 7; // Adjust for Monday start
+      $(".calendar-body-header").append($(sunday));
+   }
+
+   let weekCount = 6;
+   let dayCount = -firstDay;
+   const daysInMonth = new Date(year, month + distance + 1, 0).getDate();
+   const daysInPreviousMonth = new Date(year, month + distance, 0).getDate();
+
+   if (firstDay + daysInMonth < 36) {
+      weekCount = 5;
+   }
+
+   const day = $('<div class="day">');
+   const week = $('<div class="week">');
+
+   for (let w = 0; w < weekCount; w++) {
+      const currentWeek = week.clone();
+      for (let d = 0; d < 7; d++) {
+         const currentDay = day.clone();
+         dayCount++;
+
+         //current month
+         let dayText = dayCount;
+         let monthText = monthNo + 1;
+         let yearText = yearNo;
+         //previous month
+         if (dayCount <= 0) {
+            dayText = daysInPreviousMonth + dayCount;
+            monthText = monthNo;
+            currentDay.addClass("prev");
+            //previous year
+            if (monthNo === 0) {
+               yearText = yearNo - 1;
+               monthText = 12;
+            }
+         }
+         //next month
+         if (dayCount > daysInMonth) {
+            dayText = dayCount - daysInMonth;
+            monthText = monthNo + 2;
+            currentDay.addClass("next");
+            //next year
+            if (monthNo === 11) {
+               yearText = yearNo + 1;
+               monthText = 1;
+            }
+         }
+
+         const daySpan = $("<span>").text(dayText);
+         currentDay
+            .append(daySpan)
+            .attr("data-date", `${dayText}-${monthText}-${yearText}`);
+
+         currentWeek.append(currentDay);
+      }
+      $(calendar).append(currentWeek);
+   }
+
+   if (distance === 0) {
+      $(calendar)
+         .find(`.day[data-date="${dayNo}-${month + 1}-${year}"]`)
+         .addClass("active today");
+   }
+   $(calendar).attr("distance", distance);
 }
 
 function setTimeframe(e) {
@@ -67,4 +126,16 @@ function setTimeframe(e) {
    $(content).attr("view", timeframe);
 
    setCookie("timeframe", timeframe, 365);
+}
+
+function changeDay(e) {
+   let target = $(e.target);
+   if (target.is("span")) {
+      target = target.parent();
+   }
+   if (!target.hasClass("day")) {
+      return;
+   }
+   $(calendar).find(".active").removeClass("active");
+   target.addClass("active");
 }
